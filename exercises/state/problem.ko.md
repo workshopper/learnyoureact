49760fc5659171f127ad9dadbb2f172fe6bf6efe
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-10-30 17:51:18 +0900

    Change state to use ES6

--- exercises/state/problem.ko.md
+++ exercises/state/problem.ko.md
@@ -15,38 +15,45 @@
 
 
 ```
-var React = require('react');
+import React from 'react';
 
-var TodoBox = React.createClass({
+export default class TodoBox extends React.Component {
   // 생략
-});
+}
 
-var TodoList = React.createClass({
+class TodoList extends React.Component {
   // 생략
-});
+}
 
-var Todo = React.createClass({
-  propTypes: {
-    title: React.PropTypes.string.isRequired
-  },
-  render: function() {
-    return (
-      <tr>
-        <td style={{border: "1px solid black"}}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
-      </tr>
-    );
-  }
-});
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {checked: false};
+    }
+    propTypes:{
+        title: React.PropTypes.string.isRequired
+    }
 
+    handleChange(e) {
+        this.setState({checked: e.target.checked});
+    }
 
+    render() {
+        return (
+            <tr>
+                <td style={{border: "1px solid black"}}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                </td>
+                <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                <td style={{border: "1px solid black"}}>{this.props.children}</td>
+            </tr>
+        );
+    }
+}
 
-var TodoForm = React.createClass({
+class TodoForm extends React.Component {
   // 생략
-});
-
-module.exports = TodoBox;
+}
 ```
 
 `Todo` 안에 `checked`의 초기값을 지정하는 곳과 `handleChange`을 정의하세요.
--- exercises/state/solution/views/index.jsx
+++ exercises/state/solution/views/index.jsx
@@ -1,63 +1,64 @@
-var React = require('react');
+import React from 'react';
 
-var TodoBox = React.createClass({
-  render: function() {
-      return (
-        <div className="todoBox">
-          <h1>Todos</h1>
-	  <TodoList />
-	  <TodoForm />
-        </div>
-      );
-  }
-});
+export default class TodoBox extends React.Component {
+    render() {
+        return (
+            <div className="todoBox">
+                <h1>Todos</h1>
+                <TodoList />
+                <TodoForm />
+            </div>
+        );
+    }
+}
 
-var TodoList = React.createClass({
-  render: function() {
-    return (
-      <div className = "todoList">
-        <table style={{border: "2px solid black"}}>
-          <tbody>
-            <Todo title="Shopping">Milk</Todo>
-            <Todo title="Hair cut">13:00</Todo>
-          </tbody>
-        </table>
-      </div>    
-    );
-  }
-});
+class TodoList extends React.Component {
+    render() {
+        return (
+            <div className="todoList">
+                <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
 
-var Todo = React.createClass({
-  propTypes: {
-    title: React.PropTypes.string.isRequired	 
-  },
-  getInitialState: function() {
-    return {
-      checked: false
-    };
-  },
-  handleChange: function(e) {
-    this.setState({checked: e.target.checked});
-  },
-  render: function() {
-    return (
-      <tr>
-        <td style={{border: "1px solid black"}}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
-      </tr>
-    );	 
-  }
-});
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {checked: false};
+    }
+    propTypes:{
+        title: React.PropTypes.string.isRequired
+    }
 
-var TodoForm = React.createClass({
-  render: function() {
-    return (
-      <div className = "todoForm">
-        I am a TodoForm.
-      </div>	
-    );
-  }
-});
+    handleChange(e) {
+        this.setState({checked: e.target.checked});
+    }
 
-module.exports = TodoBox;
+    render() {
+        return (
+            <tr>
+                <td style={{border: "1px solid black"}}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                </td>
+                <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                <td style={{border: "1px solid black"}}>{this.props.children}</td>
+            </tr>
+        );
+    }
+}
+
+class TodoForm extends React.Component {
+    render() {
+        return (
+            <div className="todoForm">
+                I am a TodoForm.
+            </div>
+        );
+    }
+}
