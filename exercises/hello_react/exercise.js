e752ed1b0456b9fbbc1559b311131ca47fbc2818
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-11-14 23:30:03 +0900

    Try to change usage of babel

--- exercises/hello_react/exercise.js
+++ exercises/hello_react/exercise.js
@@ -62,7 +62,7 @@ exercise.addProcessor(function (mode, callback) {
         this.solutionStdout = through2();
     }
 
-    setTimeout(query.bind(this, mode), 100);
+    setTimeout(query.bind(this, mode), 5000);
 
     process.nextTick(function () {
         callback(null, true);
